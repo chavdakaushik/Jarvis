9b24f092d55a4ad490db7ac80aecf2f9a0bcda41
Chavda Kaushik <chavdakaushik33@gmail.com>
2020-06-28 11:49:35 +0530

    Code Update

--- Jarvis/jarvis.py
+++ Jarvis/jarvis.py
@@ -1,14 +1,15 @@
-import pyttsx3 # use for speaking # pip3 install pyttsx3
-import datetime # use to get date time
-import speech_recognition as sr # pip3 install speechRecognition
-import wikipedia # pip3 install wikipedia
-import webbrowser #pip3 install webbrowser
+import pyttsx3  # use for speaking # pip3 install pyttsx3
+import datetime  # use to get date time
+import speech_recognition as sr  # pip3 install speechRecognition
+import wikipedia  # pip3 install wikipedia
+import webbrowser  # pip3 install webbrowser
 import os
-import subprocess # for playing music 
-import smtplib # use for sending mail
+import subprocess  # for playing music
+import smtplib  # use for sending mail
 import sys
 
 engine = pyttsx3.init()
+
 
 def wishMe():
     '''When Jarvis Starts'''
@@ -20,19 +21,21 @@ def wishMe():
         speak('Good Afternoon sir')
     else:
         speak('Good Evening sir')
-    
+
     speak('I am Jarvis . How can i help you')
+
 
 def speak(audio):
     '''It will speak whatever we give input'''
     engine.say(audio)
     engine.runAndWait()
 
+
 def takeCommand():
     '''It takes microphone input from users and returns string output'''
-    
-    r = sr.Recognizer()# it will use to recognize the voices
-    
+
+    r = sr.Recognizer()  # it will use to recognize the voices
+
     with sr.Microphone() as source:
         print('Listing..')
         r.pause_threshold = 1
@@ -47,6 +50,7 @@ def takeCommand():
         return 'None'
     return query
 
+
 def sendEmail(to, content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
@@ -54,16 +58,17 @@ def sendEmail(to, content):
     server.login('youremail@gmail.com', 'yourpassword')
     server.sendmail('youremail@gmail.com', to, content)
     server.close()
-    
+
+
 if __name__ == '__main__':
     # speak('Hello , Kaushik Chavda, how are you!')
-    
+
     if 1:
         wishMe()
         query = takeCommand().lower()
         # query = 'time'
-        
-        #Logic for executing tasks on query
+
+        # Logic for executing tasks on query
 
         if 'wikipedia' in query:
             speak('Searching Wikipedia...')
@@ -75,19 +80,19 @@ if __name__ == '__main__':
 
         elif 'open youtube' in query:
             webbrowser.open('youtube.com')
-        
+
         elif 'play music' in query:
             music_dir = '//home//kaushik//Music'
             songs = os.listdir(music_dir)
             print(songs)
             os.startfile(os.path.join(music_dir, songs[0]))
-            # subprocess.call([music_dir, songs[0]])        
-        
+            # subprocess.call([music_dir, songs[0]])
+
         elif 'time' in query:
             strTime = datetime.datetime.now()
             print(strTime)
             speak(f'Sir,the time is ....{strTime}')
-     
+
         elif 'mail to kaushik' in query:
             try:
                 speak('What should i write in e-mail?')
@@ -95,10 +100,10 @@ if __name__ == '__main__':
                 to = 'chavdakaushik33@gmail.com'
                 sendEmail(to, content)
                 speak('E-mail has been send!!')
-          
+
             except Exception as e:
                 print(e)
                 speak('Sorry sir , I am not able to send E-mail..')
-        
+
         elif 'stop' in query:
             sys.exit()
